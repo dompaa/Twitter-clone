2eb56537160ac375c7022405cab213096ce734af
Dominik <gradiskid@gmail.com>
2022-06-29 21:23:08 +0200

    structured containers/sections

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,23 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Sidebar from '../components/Sidebar'
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="">
       <Head>
         <title>Twitter 2.0</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>Testing</h1>
+      <main>
+        <Sidebar />
+
+        {/* Feed */}
+        
+        {/* Widgets */}
+      </main>
 
       
     </div>
