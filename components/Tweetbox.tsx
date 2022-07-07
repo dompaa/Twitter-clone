e07b9155cfbcf5f96cb00bdfb6272fbf8a809e63
Dominik <gradiskid@gmail.com>
2022-07-07 14:51:57 +0200

    added disabled button without text message & form styling

--- components/Tweetbox.tsx
+++ components/Tweetbox.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
     CalendarIcon,
     EmojiHappyIcon,
@@ -8,28 +8,37 @@ import {
 } from '@heroicons/react/outline'
 
 function Tweetbox() {
+
+    const [input, setInput] = useState<string>('')
+
   return (
-    <div>
+    <div className='flex space-x-2 p-5'>
         <img 
         className='mt-4 h-14 w-14 rounded-full object-cover' 
         src="https://links.papareact.com/gll" 
         alt="" 
         />
 
-        <div>
-            <form>
-                <input type="text" placeholder="What's happening?" />
-                <div>
-                    <div className='flex space-x-2 text-twitter'>
+        <div className='flex flex-1 items-center pl-2'>
+            <form className='flex flex-1 flex-col'>
+                <input 
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+                type="text"
+                placeholder="What's happening?"
+                className='h-24 w-full text-xl outline-none placeholder:text-xl'/>
+                <div className='flex items-center'>
+                    <div className='flex flex-1 space-x-2 text-twitter'>
                         {/* Icons */}
-                        <PhotographIcon className='h-5 w-5'/>
+                        <PhotographIcon className='h-5 w-5 cursor-pointer transition-transform duration-150 ease-out hover:scale-150'/>
                         <SearchCircleIcon className='h-5 w-5'/>
                         <EmojiHappyIcon className='h-5 w-5'/>
                         <CalendarIcon className='h-5 w-5'/>
                         <LocationMarkerIcon className='h-5 w-5'/>
                     </div>
                     
-                    <button>Tweet</button>
+                    <button disabled={!input} className='text-white bg-twitter px-5 py-2 font-bold rounded-full
+                    disabled:opacity-50'>Tweet</button>
                 </div>
             </form>
         </div>
