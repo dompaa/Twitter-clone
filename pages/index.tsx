050019ab251673edbc338796700d6f685f6b41bc
Dominik <gradiskid@gmail.com>
2022-07-09 00:06:29 +0200

    fixed tweet not showing from cms

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,21 @@
-import type { NextPage } from 'next'
+import type { GetServerSideProps, NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import Feed from '../components/Feed'
 import Sidebar from '../components/Sidebar'
 import Widgets from '../components/Widgets'
+import { Tweet } from '../typings'
+import { fetchTweets } from '../utils/fetchTweets'
 
-const Home: NextPage = () => {
+interface Props {
+  tweets: Tweet[]
+}
+
+const Home = ({ tweets }: Props) => {
+
+  // for testing tweet if passes
+  //console.log(tweets);
+  
   return (
     <div className="lg:max-w-6xl mx-auto max-h-screen overflow-hidden">
       <Head>
@@ -17,7 +27,7 @@ const Home: NextPage = () => {
         <Sidebar />
 
        
-        <Feed/>
+        <Feed tweets={tweets}/>
         
         <Widgets/>
       </main>
@@ -28,3 +38,14 @@ const Home: NextPage = () => {
 }
 
 export default Home
+
+//server side rendering
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const tweets = await fetchTweets();
+
+  return {
+    props: {
+      tweets,
+    }
+  }
+}
--- sanity.js
+++ sanity.js
@@ -1,13 +1,12 @@
-import {
-    createClient,
-} from 'next-sanity'
+import { createClient } from 'next-sanity'
 
+// lib/config.js
 export const config = {
     dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
     projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-    apiVersion: '2021-03-25',
+    apiVersion: '2021-10-21', // Learn more: https://www.sanity.io/docs/api-versioning
     useCdn: process.env.NODE_ENV === 'production',
-}
+  }
 
 // Set up the client for fetching data in the getProps page functions, sanity's back-end
 export const sanityClient = createClient(config)
