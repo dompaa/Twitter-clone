171a0e3a0fd80dca3cd529ec08a590f0b158cf41
Dominik <gradiskid@gmail.com>
2022-07-11 23:22:27 +0200

    added react hot toast notifications

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { GetServerSideProps, NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import { Toaster } from 'react-hot-toast'
 import Feed from '../components/Feed'
 import Sidebar from '../components/Sidebar'
 import Widgets from '../components/Widgets'
@@ -22,7 +23,7 @@ const Home = ({ tweets }: Props) => {
         <title>Twitter 2.0</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      <Toaster/>
       <main className='grid grid-cols-9'>
         <Sidebar />
 
