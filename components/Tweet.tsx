c0ee55df49b52f90ed4890dd7373366124be541e
Dominik <gradiskid@gmail.com>
2022-07-17 02:02:08 +0200

    add comment box section

--- components/Tweet.tsx
+++ components/Tweet.tsx
@@ -8,6 +8,7 @@ import {
   UploadIcon,
 } from '@heroicons/react/outline'
 import { fetchComments } from '../utils/fetchComments'
+import { useSession } from 'next-auth/react'
 
 interface Props {
     tweet: Tweet
@@ -16,93 +17,134 @@ interface Props {
 function Tweet({tweet}: Props) {
   //responsible for fetching the comments useEffect
   const [comments, setComments] = useState<Comment[]>([])
+  const [commentBoxVisible, setCommentBoxVisible] = useState<boolean>(false)
+  const [input, setInput] = useState<string>('')
+  const { data: session} = useSession();
 
   const refreshComments = async () => {
     const comments: Comment[] = await fetchComments(tweet._id)
     setComments(comments);
   }
+  
 
   //when the component mounts refresh the comments (only once)
   useEffect(() => {
     refreshComments();
   }, [])
 
-  console.log(comments);
+//  console.log(comments);
   
+const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  
+}
+
 
 
   return (
-    <div className='flex flex-col space-x-3 border-y p-5 border-gray-100'>
-        <div className='flex space-x-3'>
-          <img className='w-12 h-12 rounded-full object-cover' src={tweet.profileImg} alt="" />
+    <div
+      key={tweet._id}
+      className="flex flex-col space-x-3 border-y border-gray-100 p-5"
+    >
+      <div className="flex space-x-3">
+        <img
+          className="h-10 w-10 rounded-full object-cover"
+          src={tweet.profileImg || 'https://links.papareact.com/gll'}
+          alt=""
+        />
 
-          <div>
-            <div className='flex items-center space-x-1'>
-              <p className='mr-1 font-bold'>{tweet.username}</p>
-              <p className='hidden text-sm text-gray-500 sm:inline'>
-                @{tweet.username.replace(/\s+/g, '').toLowerCase()} -</p>
+        <div>
+          <div className="flex items-center space-x-1">
+            <p className="mr-1 font-bold">{tweet.username}</p>
+            <p className="hidden text-sm text-gray-500 sm:inline">
+              @{tweet.username.replace(/\s+/g, '').toLowerCase()} ·
+            </p>
 
-              <TimeAgo
-              className='text-sm text-gray-500'
-                date={tweet._createdAt}
-              />
-            </div>
-
-            
-            <p className='pt-1'>{tweet.text}</p>
-
-            {tweet.image && (
-              <img src={tweet.image} alt="" className='mr-5 ml-0 mb-1 max-h-60 rounded-lg object-cover shadow-sm' />
-            )}
+            <TimeAgo
+              className="text-sm text-gray-500"
+              date={tweet._createdAt}
+            />
           </div>
+
+          <p className="pt-1">{tweet.text}</p>
+
+          {tweet.image && (
+            <img
+              src={tweet.image}
+              className="m-5 ml-0 mb-1 max-h-60  rounded-lg object-cover shadow-sm"
+              alt=""
+            />
+          )}
         </div>
+      </div>
 
-        <div className='mt-5 flex justify-between'>
-            <div className='flex cursor-pointer space-x-3 text-gray-400'>
-              <ChatAlt2Icon className='h-5 w-5'/>
-              <p>{comments.length}</p>
-            </div>
-            
-            <div className='flex cursor-pointer space-x-3 text-gray-400'>
-              <HeartIcon className='h-5 w-5'/>
-            </div>
-            <div className='flex cursor-pointer space-x-3 text-gray-400'>
-              <SwitchHorizontalIcon className='h-5 w-5'/>
-            </div>
-            <div className='flex cursor-pointer space-x-3 text-gray-400'>
-              <UploadIcon className='h-5 w-5'/>
-            </div>
+      <div className="mt-5 flex justify-between">
+        <div
+          onClick={(e) => session && setCommentBoxVisible(!commentBoxVisible)}
+          className="flex cursor-pointer items-center space-x-3 text-gray-400"
+        >
+          <ChatAlt2Icon className="h-5 w-5" />
+          <p>{comments.length}</p>
         </div>
-        
-        {/* Comment box logic */}
+        <div className="flex cursor-pointer items-center space-x-3 text-gray-400">
+          <SwitchHorizontalIcon className="h-5 w-5" />
+        </div>
+        <div className="flex cursor-pointer items-center space-x-3 text-gray-400">
+          <HeartIcon className="h-5 w-5" />
+        </div>
+        <div className="flex cursor-pointer items-center space-x-3 text-gray-400">
+          <UploadIcon className="h-5 w-5" />
+        </div>
+      </div>
 
+      {commentBoxVisible && (
+        <form
+        onSubmit={handleSubmit} 
+        className="mt-3 flex space-x-3">
+          <input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            className="flex-1 rounded-lg bg-gray-100 p-2 outline-none"
+            type="text"
+            placeholder="Write a comment..."
+          />
+          <button
+            disabled={!input}
+            className="text-twitter disabled:text-gray-200"
+            type="submit"
+          >
+            Post
+          </button>
+        </form>
+      )}
 
-        {comments?.length > 0 && (
-          <div className='my-2 mt-5 max-h-44 space-y-0 overflow-y-scroll border-t border-gray-100 p-5'>
-            {comments.map(comment => (
-              <div key={comment._id} className='relative flex space-x-2 space-y-4'>
-                <hr className='absolute left-5 top-10 h-8 border-x border-twitter/30'/>
-                <img 
-                src={comment.profileImg} 
-                className="mt-2 w-7 h-7 object-cover rounded-full" 
-                alt="" />
-                <div>
-                  <div className='flex items-center space-x-1'>
-                    <p className='mr-1 font-bold'>{comment.username}</p>
-                    <p className='hidden text-sm text-gray-500 lg:inline'>
-                      @{comment.username.replace(/\s+/g, '').toLowerCase()} - </p>
+      {comments?.length > 0 && (
+        <div className="my-2 mt-5 max-h-44 space-y-5 overflow-y-scroll border-t border-gray-100 p-5">
+          {comments.map((comment) => (
+            <div key={comment._id} className="relative flex space-x-2">
+              <hr className="absolute left-5 top-10 h-8 border-x border-twitter/30" />
+              <img
+                src={comment.profileImg}
+                className="mt-2 h-7 w-7 rounded-full object-cover"
+                alt=""
+              />
+              <div>
+                <div className="flex items-center space-x-1">
+                  <p className="mr-1 font-bold">{comment.username}</p>
+                  <p className="hidden text-sm text-gray-500 lg:inline">
+                    @{comment.username.replace(/\s+/g, '').toLowerCase()} ·
+                  </p>
+
                   <TimeAgo
-                    className='text-sm text-gray-500'
+                    className="text-sm text-gray-500"
                     date={comment._createdAt}
                   />
-                  </div>
-                <p>{comment.comment}</p>
-
                 </div>
+                <p>{comment.comment}</p>
               </div>
-            ))}
-          </div>
-        )}
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   )
 }
